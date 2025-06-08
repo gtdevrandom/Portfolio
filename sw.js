5d0036f2f188e206ed38e66b17dd5284e7281993
gtdev <gtdevrandom@gmail.com>
2025-06-08 14:16:13 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+// Changer ceci de votre nom de dépôt
+var GHPATH =' '/Portfolio' ;
+ 
+// Choisir un préfixe d'application différent
+var APP-PREFIX ='Portfolio' ;
+ 
+// La version du cache. Chaque fois que vous changez l'un des fichiers
+// vous devez changer cette version (version-01, version-02...). 
+// Si vous ne changez pas la version, le travailleur de service donnera votre
+// utilisateurs les anciens fichiers
+var VERSION - 'version-00' ;
+ 
+// Les fichiers à mettre à disposition pour une utilisation hors ligne. Assurez-vous d'ajouter 
+// autres membres de cette liste
+URLS VA=[    
+  {}-GHPATH/,,
+  {}/GHPATH/index.html,
+  {}GHPATH/ css / styles.css,
+  {}GHPATH/ js / app.js
+-
